e33a2a345bc90d89eef61e297f4851a97c38dd40
Jinglin Tan <lkluckly009@gmail.com>
2023-03-27 21:49:31 -0400

    Add question 268

---
+++ 268_Missing_Number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+
+        missing = 0
+
+        for i in range(len(nums)):
+            missing = missing ^ i ^ nums[i]
+
+        return missing ^ len(nums)
